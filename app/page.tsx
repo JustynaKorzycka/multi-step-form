3c666e8ef6fca5472fc7618f16ec348b4b4ce660
Justyna <justyna.korzycka@gmail.com>
2024-01-26 10:48:24 +0100

    add content wrapper, stepsSection and form to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
+"use client";
+
+import ContentWrapper from "@/components/ContentWrapper/ContentWrapper";
+import Form from "@/components/Form/Form";
+import StepSection from "@/components/StepSection/StepSection";
+import { useState } from "react";
+
 export default function Home() {
- return <></>;
+ const [currentStep, setCurrentStep] = useState(0);
+
+ return (
+  <ContentWrapper>
+   <div className="container">
+    <StepSection currentStep={currentStep} />
+    <Form />
+   </div>
+  </ContentWrapper>
+ );
 }
