513cda6d6a1d9b481a6de569ed372cabf4dfad32
Justyna <justyna.korzycka@gmail.com>
2024-01-26 10:35:55 +0100

    create step data file

---
+++ data/stepData.ts
@@ -0,0 +1,8 @@
+const STEP_DATA = [
+ { number: 1, title: "Step 1", desc: "Your info" },
+ { number: 2, title: "Step 2", desc: "Select plan" },
+ { number: 3, title: "Step 3", desc: "Add-ons" },
+ { number: 4, title: "Step 4", desc: "Summary" },
+];
+
+export default STEP_DATA;
