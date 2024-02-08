17722c01f3514923ac0135a7cd0409de06abe682
Justyna <justyna.korzycka@gmail.com>
2024-02-08 23:10:43 +0100

    create context for user data and step info and actions

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Ubuntu } from "next/font/google";
 import StyledComponentsRegistry from "@/lib/registry";
 import GlobalStyles from "@/styles/GlobalStyles";
 import ThemeClient from "@/lib/ThemeClient";
+import { MultiFormContextProvider } from "@/context/multistepFormContext";
 
 const ubuntu = Ubuntu({ subsets: ["latin"], weight: "400" });
 
@@ -17,13 +18,15 @@ export default function RootLayout({
  children: React.ReactNode;
 }>) {
  return (
-  <html lang="en">
-   <StyledComponentsRegistry>
-    <ThemeClient>
-     <GlobalStyles />
-     <body className={ubuntu.className}>{children}</body>
-    </ThemeClient>
-   </StyledComponentsRegistry>
-  </html>
+  <MultiFormContextProvider>
+   <html lang="en">
+    <StyledComponentsRegistry>
+     <ThemeClient>
+      <GlobalStyles />
+      <body className={ubuntu.className}>{children}</body>
+     </ThemeClient>
+    </StyledComponentsRegistry>
+   </html>
+  </MultiFormContextProvider>
  );
 }
---
+++ context/multistepFormContext.tsx
@@ -0,0 +1,84 @@
+"use client";
+import STEP_DATA from "@/data/stepData";
+import { useContext, useState, createContext, ReactNode } from "react";
+import { PlansEnum } from "@/data/planData";
+
+export interface IPersonalInfo {
+ userName: string;
+ email: string;
+ phone: string;
+}
+
+export interface User extends IPersonalInfo {
+ plan: PlansEnum;
+ monthly: boolean;
+ addOns: string;
+}
+
+export const initialUserData: User = {
+ userName: "",
+ email: "",
+ phone: "",
+ plan: PlansEnum.arcade,
+ monthly: true,
+ addOns: "",
+};
+
+export interface UserContextType {
+ user: User;
+ updateUserData: (property: Partial<User>) => void;
+ currentStep: number;
+ stepsNumber: number;
+ handlePrevStep: () => void;
+ handleNextStep: () => void;
+}
+
+export const MultiFormContext = createContext<UserContextType>({
+ user: initialUserData,
+ updateUserData: () => {},
+ currentStep: 0,
+ stepsNumber: 0,
+ handlePrevStep: () => {},
+ handleNextStep: () => {},
+});
+
+interface MultiFormProps {
+ children: ReactNode;
+}
+
+export const MultiFormContextProvider = ({ children }: MultiFormProps) => {
+ const [user, setUser] = useState<User>(initialUserData);
+ const [currentStep, setCurrentStep] = useState<number>(0);
+
+ const updateUserData = (values: Partial<User>) => {
+  setUser({ ...user, ...values });
+ };
+ const stepsNumber = STEP_DATA.length;
+
+ const handlePrevStep = () => {
+  if (currentStep > 0) {
+   setCurrentStep((prev) => prev - 1);
+  }
+ };
+ const handleNextStep = () => {
+  if (currentStep < 3) {
+   setCurrentStep((prev) => prev + 1);
+  }
+ };
+ return (
+  <MultiFormContext.Provider
+   value={{
+    user,
+    updateUserData,
+    currentStep,
+    stepsNumber,
+    handlePrevStep,
+    handleNextStep,
+   }}
+  >
+   {children}
+  </MultiFormContext.Provider>
+ );
+};
+
+export const useMultiFormContext = () => useContext(MultiFormContext);
