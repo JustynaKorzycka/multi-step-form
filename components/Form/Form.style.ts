0588e692493b886182259a86ef259a09ea49025b
Justyna <justyna.korzycka@gmail.com>
2024-02-08 23:12:37 +0100

    refactor Form component, add styles

--- components/Form/Form.style.ts
+++ components/Form/Form.style.ts
@@ -2,11 +2,12 @@
 
 import styled from "styled-components";
 
-export const StyledForm = styled.form`
+export const FormWrapper = styled.div`
  flex-grow: 1;
  display: flex;
  flex-direction: column;
  justify-content: space-between;
+ width: 100%;
 
  @media screen and (min-width: ${({ theme }) => theme.sizes.sm}px) {
   max-width: 450px;
@@ -15,13 +16,25 @@ export const StyledForm = styled.form`
  }
 `;
 
-export const FormSection = styled.div`
+export const Form = styled.form`
  background-color: ${({ theme }) => theme.colors.white};
  border-radius: ${({ theme }) => theme.borderRadius.lg};
  padding: 2rem 1.5rem;
  -webkit-box-shadow: 0px 25px 40px -20px rgba(0, 0, 0, 0.09);
  -moz-box-shadow: 0px 25px 40px -20px rgba(0, 0, 0, 0.09);
  box-shadow: 0px 25px 40px -20px rgba(0, 0, 0, 0.09);
+
+ p.topPar {
+  margin-bottom: 1.4rem;
+ }
+
+ @media screen and (min-width: ${({ theme }) => theme.sizes.sm}px) {
+  box-shadow: none;
+
+  p.topPar {
+   margin-bottom: 2.2rem;
+  }
+ }
 `;
 
 export const ButtonsSection = styled.div`
--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -1,43 +1,26 @@
+"use client";
 import PersonalForm from "./PersonalForm";
 import PlanForm from "./PlanForm";
 import AddOnsForm from "./AddOnsForm";
 import SummaryForm from "./SummaryForm";
-import { StyledForm, FormSection, ButtonsSection } from "./Form.style";
+import { FormWrapper } from "./Form.style";
 import { useMemo } from "react";
-import Button from "../Button/Button";
+import { useMultiFormContext } from "@/context/multistepFormContext";
 
-interface IFormProps {
- currentStep: number;
- onNextStep: () => void;
- onPrevStep: () => void;
- stepsNumber: number;
-}
+const Form = () => {
+ const { currentStep } = useMultiFormContext();
 
-const Form = ({
- currentStep,
- onNextStep,
- onPrevStep,
- stepsNumber,
-}: IFormProps) => {
  const activeForm = useMemo(() => {
   if (currentStep === 0) return <PersonalForm />;
   else if (currentStep === 1) return <PlanForm />;
   else if (currentStep === 2) return <AddOnsForm />;
   else return <SummaryForm />;
  }, [currentStep]);
+
  return (
-  <StyledForm>
-   <FormSection>{activeForm}</FormSection>
-   <ButtonsSection>
-    <Button kind={currentStep === stepsNumber - 1 ? "confirm" : "next"} />
-    {currentStep > 0 && (
-     <Button
-      kind="prev"
-      onClick={onPrevStep}
-     />
-    )}
-   </ButtonsSection>
-  </StyledForm>
+  <FormWrapper>
+   <FormWrapper>{activeForm}</FormWrapper>
+  </FormWrapper>
  );
 };
 
