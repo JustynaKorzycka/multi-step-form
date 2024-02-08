342a388205816d31278de468bd0df130aeb264b4
Justyna <justyna.korzycka@gmail.com>
2024-02-08 23:12:03 +0100

    refactor main page, delete states

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,15 @@
 "use client";
-import { useCallback, useState } from "react";
 import ContentWrapper from "@/components/ContentWrapper/ContentWrapper";
 import StepSection from "@/components/StepSection/StepSection";
-import STEP_DATA from "@/data/stepData";
+
 import Form from "@/components/Form/Form";
 
 export default function Home() {
- const [currentStep, setCurrentStep] = useState(3);
-
- const undoStepHandler = useCallback(() => {
-  if (currentStep > 0) {
-   setCurrentStep((prev) => prev - 1);
-  }
- }, [currentStep]);
-
- const nextStepHandler = useCallback(() => {
-  if (currentStep < STEP_DATA.length - 1) {
-   setCurrentStep((prev) => prev + 1);
-  }
- }, [currentStep]);
-
  return (
   <ContentWrapper>
    <div className="container">
-    <StepSection currentStep={currentStep} />
-    <Form
-     currentStep={currentStep}
-     onNextStep={nextStepHandler}
-     onPrevStep={undoStepHandler}
-     stepsNumber={STEP_DATA.length}
-    />
+    <StepSection />
+    <Form />
    </div>
   </ContentWrapper>
  );
